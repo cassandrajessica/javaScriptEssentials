42b3589c704fd173da8b4f69625b0261643ffe8f
cassandrajessica <clehmanvo2533@collin.edu>
2025-05-20 17:22:30 -0400

    function hosting

---
+++ followersCount/followers_count.js
@@ -0,0 +1,23 @@
+// starting point counter
+let count = 0;
+
+// function increases follower count
+function increaseCount() {
+    count++;
+    displayCount(); // hosting example
+    checkCountValue();  // hosting example
+}
+
+// will display followers through html found on <p>
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML = count;
+}
+
+//function to send message based on followers gained
+function checkCountValue() {
+    if(count === 10) {
+        alert("Your insta post gained 10 followers! Congrats!");
+    } else if(count === 20) {
+        alert("Your insta post gained 20 followers! Nice Job!");
+    }
+}
