5cf422185dff1232a0fe578d32532487fcafa62f
cassandrajessica <clehmanvo2533@collin.edu>
2025-05-20 15:52:05 -0400

    more control flow and conditionals

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -44,3 +44,9 @@ switch (userType) {
 }
 
 console.log("User category: ", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
